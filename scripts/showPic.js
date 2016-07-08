349d36084c727db1728765f8190b9156b9daadd8
myin75 <yinjun75@gmail.com>
2016-07-08 10:06:14 +1000

    finish learning dom scripting

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -102,6 +102,37 @@ function addPlaceHolder() {
     //document.body.appendChild(description);
 }
 
+function positionElement() {
+    var elem = document.getElementById("shit");
+    elem.style.position = "absolute";
+    elem.style.left = "50px";
+    elem.style.top = "100px";
+    moveElement("shit", 200,200,20);
+}
 
-addLoadEvent(addPlaceHolder);
-addLoadEvent(prepLnks);
+function moveElement(elementID, final_x, final_y, interval) {
+    var elem = document.getElementById(elementID);
+    
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    
+    if ( (xpos == final_x) && (ypos == final_y)) {
+        return true;
+    }
+    if (xpos < final_x)
+        xpos++;
+    else if (xpos > final_x)
+        xpos--;
+    if (ypos < final_y)
+        ypos++;
+    else if (ypos < final_y)
+        ypos--;
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    movement = setTimeout(repeat, interval);
+}
+
+//addLoadEvent(addPlaceHolder);
+//addLoadEvent(prepLnks);
+addLoadEvent(positionElement);
